f77776919136a6000d4c48f7922fbfe550b9d169
Dohyeok Kim <hyeokdokim@gmail.com>
2025-09-29 21:39:48 +0900

    [#16] 최상단 화면 네비게이션 추가

--- app/src/main/java/com/hyeok/recipebook/presentation/MainActivity.kt
+++ app/src/main/java/com/hyeok/recipebook/presentation/MainActivity.kt
@@ -6,11 +6,24 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarDefaults
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavDestination.Companion.hasRoute
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.hyeok.recipebook.presentation.navigation.Route
 import com.hyeok.recipebook.presentation.ui.theme.RecipeBookTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -21,29 +34,65 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             RecipeBookTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainView()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainView() {
+    val navController = rememberNavController()
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        bottomBar = {
+            NavigationBar(windowInsets = NavigationBarDefaults.windowInsets) {
+                val navBackStackEntry by navController.currentBackStackEntryAsState()
+                val currentDestination = navBackStackEntry?.destination
+
+                Route.topLevelRoutes.forEachIndexed { idx, route ->
+                    NavigationBarItem(
+                        selected = currentDestination?.hierarchy?.any { it.hasRoute(route.route::class) } == true,
+                        onClick = {
+                            navController.navigate(route.route) {
+                                popUpTo(navController.graph.findStartDestination().id) {
+                                    saveState = true
+                                }
+
+                                launchSingleTop = true
+                                restoreState = true
+                            }
+                        },
+                        icon = {Icon(route.icon, contentDescription = null)},
+                        label = {Text(text = route.name)}
+                    )
+                }
+            }
+        }
+    ) { innerPadding ->
+        NavHost(
+            modifier = Modifier.padding(innerPadding),
+            navController = navController,
+            startDestination = Route.Home
+        ) {
+            composable<Route.Home> { backStack ->
+                HomeScreen()
+            }
+            composable<Route.Menu> { backStack ->
+                MenuScreen()
+            }
+            composable<Route.Roulette> { backStack ->
+                RouletteScreen()
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MainPreview() {
     RecipeBookTheme {
-        Greeting("Android")
+        MainView()
     }
 }
