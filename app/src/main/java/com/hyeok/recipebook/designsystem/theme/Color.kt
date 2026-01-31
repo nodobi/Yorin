e247bd5a690bced835a5e4e5e64b817959025994
Dohyeok Kim <hyeokdokim@gmail.com>
2026-01-31 14:08:30 +0900

    [#21] 색상 코드 추가

--- app/src/main/java/com/hyeok/recipebook/designsystem/theme/Color.kt
+++ app/src/main/java/com/hyeok/recipebook/designsystem/theme/Color.kt
@@ -9,3 +9,37 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+// Main
+val Main1 = Color(0xFFFF7B26)
+val Main2 = Color(0xFFFFA870)
+val Main3 = Color(0xFFFFB586)
+val Main4 = Color(0xFFFFC7A2)
+val Main5 = Color(0xFFFFD5BA)
+val Main6 = Color(0xFFFFE5D4)
+val Main7 = Color(0xFFFFEAD2)
+val Main8 = Color(0xFFFFF3E5)
+
+// SUB
+val Sub1 = Color(0xFFFF263D)
+val Sub2 = Color(0xFFFF858A)
+val Sub3 = Color(0xFFFFC1C3)
+val Sub4 = Color(0xFFFFE1E2)
+
+// Black
+val Black1 = Color(0xFF424242)
+val Black2 = Color(0xFF616161)
+val Black3 = Color(0xFF9E9E9E)
+val Black4 = Color(0xFFBBBBBB)
+val Black5 = Color(0xFFCCCCCC)
+val Black6 = Color(0xFFEEEEEE)
+val Black7 = Color(0xFFFFFFFF)
+
+// Success
+val Success1 = Color(0xFF148660)
+val Success2 = Color(0xFF18A173)
+val Success3 = Color(0xFFE8F5E9)
+
+// Dimming
+val Dimming = Color(0x80616161)
