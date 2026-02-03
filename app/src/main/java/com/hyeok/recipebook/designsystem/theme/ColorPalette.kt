ea1c55594ec56c83d3faa5726396290b65652226
Dohyeok Kim <hyeokdokim@gmail.com>
2026-02-03 14:49:11 +0900

    [#21] 색상 팔레트 추가

---
+++ app/src/main/java/com/hyeok/recipebook/designsystem/theme/ColorPalette.kt
@@ -0,0 +1,56 @@
+package com.hyeok.recipebook.designsystem.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+data class AppColorPalette(
+    val main1: Color,
+    val main2: Color,
+    val main3: Color,
+    val main4: Color,
+    val main5: Color,
+    val main6: Color,
+    val main7: Color,
+    val main8: Color,
+    val sub1: Color,
+    val sub2: Color,
+    val sub3: Color,
+    val sub4: Color,
+    val black1: Color,
+    val black2: Color,
+    val black3: Color,
+    val black4: Color,
+    val black5: Color,
+    val black6: Color,
+    val black7: Color,
+    val success1: Color,
+    val success2: Color,
+    val success3: Color,
+    val dimming: Color,
+)
+
+internal val AppLightColorPalette = AppColorPalette(
+    main1 = Main1,
+    main2 = Main2,
+    main3 = Main3,
+    main4 = Main4,
+    main5 = Main5,
+    main6 = Main6,
+    main7 = Main7,
+    main8 = Main8,
+    sub1 = Sub1,
+    sub2 = Sub2,
+    sub3 = Sub3,
+    sub4 = Sub4,
+    black1 = Black1,
+    black2 = Black2,
+    black3 = Black3,
+    black4 = Black4,
+    black5 = Black5,
+    black6 = Black6,
+    black7 = Black7,
+    success1 = Success1,
+    success2 = Success2,
+    success3 = Success3,
+    dimming = Dimming,
+)
