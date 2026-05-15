49034eb41563421aac755eb75727c9d9971943b0
Dohyeok Kim <hyeokdokim@gmail.com>
2026-05-15 14:04:12 +0900

    feat: 배경화면 있는 Preview 어노테이션 클래스 추가

---
+++ app/src/main/java/com/hyeok/recipebook/designsystem/theme/Preview.kt
@@ -0,0 +1,6 @@
+package com.hyeok.recipebook.designsystem.theme
+
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(backgroundColor = 0xFFFFFFFF, showBackground = true)
+annotation class BackgroundPreview
