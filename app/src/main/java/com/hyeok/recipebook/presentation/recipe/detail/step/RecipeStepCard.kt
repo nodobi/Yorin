9c5834a6beb1df543ef22100ff639d569e13cac2
Dohyeok Kim <hyeokdokim@gmail.com>
2026-08-03 13:26:07 +0900

    feat: 조리순서 카드, UiModel 추가

---
+++ app/src/main/java/com/hyeok/recipebook/presentation/recipe/detail/step/RecipeStepCard.kt
@@ -0,0 +1,83 @@
+package com.hyeok.recipebook.presentation.recipe.detail.step
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.hyeok.recipebook.designsystem.components.YorinCard
+import com.hyeok.recipebook.designsystem.components.YorinText
+import com.hyeok.recipebook.designsystem.theme.YorinTheme
+
+@Composable
+fun RecipeStepCard(
+    modifier: Modifier = Modifier,
+    recipeStepUiModel: RecipeStepUiModel
+) {
+    YorinCard(
+        modifier = modifier
+            .fillMaxWidth(),
+        stroke = BorderStroke(1.dp, YorinTheme.colors.black5)
+    ) {
+        Row(
+            modifier = modifier
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            StepIcon(
+                modifier = Modifier.size(32.dp),
+                order = recipeStepUiModel.order
+            )
+
+            YorinText(
+                modifier = Modifier.fillMaxWidth(),
+                text = recipeStepUiModel.description,
+                style = YorinTheme.typography.body2,
+            )
+        }
+    }
+}
+
+@Composable
+fun StepIcon(
+    modifier: Modifier,
+    order: Int
+) {
+    Box(
+        modifier = modifier
+            .background(
+                color = YorinTheme.colors.black6,
+                shape = CircleShape
+            ),
+        contentAlignment = Alignment.Center
+    ) {
+        YorinText(
+            text = "$order",
+            style = YorinTheme.typography.body4,
+            color = YorinTheme.colors.black3
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun RecipeStepCardPreview() {
+    YorinTheme {
+        RecipeStepCard(
+            modifier = Modifier.wrapContentSize(),
+            recipeStepUiModel = RecipeStepUiModel(1, 1, "엄청나게 심오하고 특별한 조리법에 대한 글이라 아무래도 길어질 수 밖에 없다 이거지")
+        )
+    }
+
+}
--- app/src/main/java/com/hyeok/recipebook/presentation/recipe/detail/step/RecipeStepUiModel.kt
+++ app/src/main/java/com/hyeok/recipebook/presentation/recipe/detail/step/RecipeStepUiModel.kt
@@ -1,6 +1,8 @@
 package com.hyeok.recipebook.presentation.recipe.detail.step
 
+// TODO:: 순서 변경은 나중에 구현
 data class RecipeStepUiModel(
+    val id: Int,
     val order: Int,
     val description: String
 )
