85f6f4e4a9600beef0bd79b4a4b1bf1f8b426df4
Dohyeok Kim <hyeokdokim@gmail.com>
2026-08-25 23:26:13 +0900

    feat: 재료 Repository 추가

---
+++ app/src/main/java/com/hyeok/recipebook/data/repository/IngredientRepository.kt
@@ -0,0 +1,8 @@
+package com.hyeok.recipebook.data.repository
+
+import com.hyeok.recipebook.presentation.ingredient.model.IngredientUiModel
+import kotlinx.coroutines.flow.Flow
+
+interface IngredientRepository {
+
+}
---
+++ app/src/main/java/com/hyeok/recipebook/data/repository/impl/IngredientRepositoryImpl.kt
@@ -0,0 +1,8 @@
+package com.hyeok.recipebook.data.repository.impl
+
+import com.hyeok.recipebook.data.repository.IngredientRepository
+import com.hyeok.recipebook.presentation.ingredient.model.IngredientUiModel
+import kotlinx.coroutines.flow.Flow
+
+class IngredientRepositoryImpl: IngredientRepository {
+}
