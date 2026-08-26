d4d2d7f2ff796e44f153fcd53cbef058e6da25aa
Dohyeok Kim <hyeokdokim@gmail.com>
2026-08-26 23:16:26 +0900

    feat: Database, Dao 추가

---
+++ app/src/main/java/com/hyeok/recipebook/data/database/AppDatabase.kt
@@ -0,0 +1,36 @@
+package com.hyeok.recipebook.data.database
+
+import android.content.Context
+import androidx.room3.Database
+import androidx.room3.Room
+import androidx.room3.RoomDatabase
+import com.hyeok.recipebook.data.database.dao.IngredientDao
+import com.hyeok.recipebook.data.database.dao.RecipeDao
+import com.hyeok.recipebook.data.database.entity.IngredientEntity
+import com.hyeok.recipebook.data.database.entity.WeightUnitEntity
+
+@Database(
+    entities = [IngredientEntity::class, WeightUnitEntity::class],
+    version = 1
+)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun ingredientDao(): IngredientDao
+    abstract fun recipeDao(): RecipeDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "ingredient_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
