5efa526bd2b09eb598f5f6b73a7e5adbe7253924
Dohyeok Kim <hyeokdokim@gmail.com>
2026-09-12 12:20:44 +0900

    feat: Timber 적용

--- app/src/main/java/com/hyeok/recipebook/RecipeBookApplication.kt
+++ app/src/main/java/com/hyeok/recipebook/RecipeBookApplication.kt
@@ -1,7 +1,40 @@
 package com.hyeok.recipebook
 
 import android.app.Application
+import android.util.Log
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class RecipeBookApplication: Application() { }
+class RecipeBookApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if(BuildConfig.DEBUG) {
+            Timber.plant(object: Timber.DebugTree() {
+                override fun createStackElementTag(element: StackTraceElement): String? {
+                    return String.format(
+                        "Class:%s: Line: %s, Method: %s",
+                        super.createStackElementTag(element),
+                        element.lineNumber,
+                        element.methodName
+                    )
+                }
+            })
+        } else {
+            Timber.plant(ReleaseTree())
+        }
+    }
+}
+
+private class ReleaseTree : Timber.Tree() {
+    override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+        if(priority == Log.VERBOSE || priority == Log.DEBUG){
+            return
+        }
+
+        // 크래시 리포트를 연결한다면 여기에
+    }
+}
+
